5871d8a8f5f09c053ce3cca533482da6ca9a28bd
Ilya <ilya.sherstyuk@gmail.com>
2019-07-13 23:27:21 -0700

    put color into biome

--- js/biome.js
+++ js/biome.js
@@ -37,3 +37,42 @@ class Biome
 	set background_color(x)  {this._background_color = x;}
 	set name(x) 		 	 {this._name = x;}
 }
+class RGBColor
+{
+	constructor(r, g, b, a=255)
+	{
+		this._r = r;
+		this._g = g;
+		this._b = b;
+		this._a = a;
+	}
+	get r() {return this._r;}
+	get g() {return this._g;}
+	get b() {return this._b;}
+	get a() {return this._a;}
+
+	set r(x) {this._r = x;}
+	set g(x) {this._g = x;}
+	set b(x) {this._b = x;}
+	set a(x) {this._a = x;}
+
+	add(color)
+	{
+		var return_color = new RGBColor(this._r, this._g, this._b, this._a)
+		return_color.r += color.r;
+		return_color.g += color.g;
+		return_color.b += color.b;
+		return_color.a += color.a;
+		return return_color;
+	}
+
+	scale(factor)
+	{
+		var return_color = new RGBColor(this._r, this._g, this._b, this._a)
+		return_color.r *= factor;
+		return_color.g *= factor;
+		return_color.b *= factor;
+		return_color.a *= factor;
+		return return_color;
+	}
+}
